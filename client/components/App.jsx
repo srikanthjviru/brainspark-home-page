8337120dba443923e12e0265898c50a9b0fdc592
Sergey.Illarionov <easthunch@gmail.com>
2016-08-05 11:30:22 +0300

    add: common code fore 'what we do section'

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.less';
 
 import WhoWeAre from './WhoWeAre.jsx';
+import WhatWeDo from './WhatWeDo.jsx';
 
 class App extends React.Component {
     render() {
         return(
             <div className='App'>
                 <WhoWeAre />
+                <WhatWeDo />
             </div>
         )
     }
