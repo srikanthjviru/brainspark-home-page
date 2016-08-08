3e5351c655fdf97ccb0649ee904971b4051d9672
Sergey.Illarionov <easthunch@gmail.com>
2016-08-08 11:39:39 +0300

    add: 'What we do' section

--- client/api/index.js
+++ client/api/index.js
@@ -3,15 +3,13 @@ import axios from 'axios';
 import { server } from '../../etc/config.json';
 
 export default {
-    getTechnologies() {
-        return axios.get(`${server.host}:${server.port}/technologies`);
+    getTechnologies(callback) {
+        axios.get(`${server.host}:${server.port}/technologies`)
+                .then((res) => {
+                    callback(null, res.data);
+                })
+                .catch((error) => {
+                    callback(error);
+                });
     }
-
-    // createNote(data) {
-    //     return axios.post(`${apiPrefix}/notes`, data);
-    // },
-    //
-    // deleteNote(noteId) {
-    //     return axios.delete(`${apiPrefix}/notes/${noteId}`);
-    // }
 }
