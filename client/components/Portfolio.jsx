921a72a3f058fdeb3ebdee633b8556a9888cd7cf
Sergey.Illarionov <easthunch@gmail.com>
2016-08-23 15:40:30 +0300

    add: filter markup

--- client/components/Portfolio.jsx
+++ client/components/Portfolio.jsx
@@ -1,15 +1,29 @@
 import React from 'react';
-import Hexagon from 'react-hexagon';
-import dotsTextureImg from '../img/dots-texture.jpg';
 
 import './Portfolio.less';
 
 class Portfolio extends React.Component {
+    static defaultProps = {
+        filterItems: ['desktop', 'web', 'mobile']
+    }
+
     render() {
         return(
             <section className='section portfolio'>
                 <div className='portfolio__body'>
-
+                    <a className="action-btn action-btn--portfolio" href="#">Send the Project</a>
+                    <h2 className='section__header section__header--portfolio'>Our works</h2>
+                    <div className="portfolio__filter">
+                        {
+                            this.props.filterItems.map((item, index) =>
+                                <div
+                                    key={index}
+                                    className={`portfolio__filter-item portfolio__filter-item--${item}`}
+                                    data-item={item}
+                                />
+                            )
+                        }
+                    </div>
                 </div>
             </section>
         )
