a295be66bf5efd3a46a4a9c6911141008c23d338
Sergey.Illarionov <easthunch@gmail.com>
2016-09-08 10:09:40 +0300

    add: footer contact form

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -2,6 +2,31 @@ import React from 'react';
 import './Footer.less';
 
 class Footer extends React.Component {
+
+    handlerFormFocus = (e) => {
+        this.removeFocusedClassInForm(e);
+        e.target.parentNode.classList.add('contact-form__input--focused');
+    }
+
+    removeFocusedClassInForm(e) {
+        const allFormsDivs = e.currentTarget.querySelectorAll('.contact-form__input');
+        allFormsDivs.forEach((element) => {
+            const inputElement = element.querySelector('input');
+            if(inputElement) {
+                if(!inputElement.value) {
+                    element.classList.remove('contact-form__input--focused');
+                }
+            }
+
+            const textareaElement = element.querySelector('textarea');
+            if(textareaElement) {
+                if(!textareaElement.value) {
+                    element.classList.remove('contact-form__input--focused');
+                }
+            }
+        })
+    }
+
     render() {
         return(
             <footer className='section footer'>
@@ -9,6 +34,29 @@ class Footer extends React.Component {
                     Contact us
                 </div>
                 <div className='footer__body'>
+                    <form
+                        className='contact-form contact-form--footer'
+                        onFocus={this.handlerFormFocus}
+                        onBlur={this.removeFocusedClassInForm}
+                    >
+                        <div className="contact-form__input contact-form__input--name">
+                            <label htmlFor="contact-form__input-name">Name<sup>*</sup></label>
+                            <input id='contact-form__input-name' type="text" name='name'/>
+                        </div>
+                        <div className="contact-form__input contact-form__input--email">
+                            <label htmlFor="contact-form__input-email">Email<sup>*</sup></label>
+                            <input id='contact-form__input-email' type="email" name='email'/>
+                        </div>
+                        <div className="contact-form__input contact-form__input--disabled">
+                            <label htmlFor="contact-form__input-subject">Subject: I need a site<sup>*</sup></label>
+                            <input id='contact-form__input-subject' type="text" name='subject' disabled />
+                        </div>
+                        <div className="contact-form__input contact-form__input--message">
+                            <label htmlFor="contact-form__input-message">tell us what site you need<sup>*</sup></label>
+                            <textarea id='contact-form__input-message' name='message'></textarea>
+                        </div>
+                        <button className='contact-form__submit-btn' type='submit'>Submit</button>
+                    </form>
                     <ul className='contacts contacts--footer'>
                         <li className="contacts__location">Ukraine, Cherkassy</li>
                         <li className="contacts__connection">
