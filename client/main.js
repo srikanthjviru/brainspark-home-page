fb7a5a4f1062119f7721bad54389519180e9f929
Sergey.Illarionov <easthunch@gmail.com>
2016-09-13 17:53:58 +0300

    add: router

--- client/main.js
+++ client/main.js
@@ -1,10 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {Router, Route, Redirect, browserHistory} from 'react-router';
 
 import './styles/main.less';
 import App from './components/App.jsx';
+import HomePage from './components/HomePage.jsx';
+import ProjectPage from './components/ProjectPage.jsx';
+
 
 ReactDOM.render(
-    <App />,
-    document.getElementById("main")
+    <Router history={browserHistory}>
+        <Redirect from="/" to="home" />
+        <Route path="/" component={App}>
+            <Route path="/home" component={HomePage} />
+            <Route path="/projects/:projectID" component={ProjectPage} />
+        </Route>
+    </Router>,
+    document.getElementById('main')
 );
