ca1d0dc59b4aafb3980fb3db7eb8205b3f2a5def
Sergey.Illarionov <easthunch@gmail.com>
2016-09-15 09:42:54 +0300

    add: api method

--- client/api/index.js
+++ client/api/index.js
@@ -20,5 +20,15 @@ export default {
                 .catch((error) => {
                     callback(error);
                 });
+    },
+
+    getProjectByID(projectID, callback) {
+        axios.get(`${server.host}:${server.port}/projects/${projectID}`)
+                .then((res) => {
+                    callback(null, res.data);
+                })
+                .catch((error) => {
+                    callback(error);
+                });
     }
 }
